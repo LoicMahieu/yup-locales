9d6f54aefe487ff804a13e5a92a3636dd7cee761
Simen A. W. Olsen <cobraz@cobraz.no>
2020-05-18 21:00:50 +0200

    feat: add de language

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as fr from './locales/fr';
 import * as nl from './locales/nl';
 import * as nb from './locales/nb';
+import * as de from './locales/de';
 
-export { fr, nl, nb };
+export { fr, nl, nb, de };
---
+++ src/locales/de.ts
@@ -0,0 +1,56 @@
+/*eslint-disable no-template-curly-in-string*/
+/**
+ * This work is derived from skress/yup-locale-de.
+ * https://github.com/skress/yup-locale-de/
+ */
+
+import { LocaleObject } from 'yup';
+
+// Based on https://github.com/jquense/yup/blob/2973d0a/src/locale.js
+export const mixed: LocaleObject['mixed'] = {
+  default: '${path} ist ungültig',
+  required: '${path} ist ein Pflichtfeld',
+  oneOf: '${path} muss einem der folgenden Werte entsprechen: ${values}',
+  notOneOf: '${path} darf keinem der folgenden Werte entsprechen: ${values}',
+  // @todo Add notType to de translation
+};
+
+export const string: LocaleObject['string'] = {
+  length: '${path} muss genau ${length} Zeichen lang sein',
+  min: '${path} muss mindestens ${min} Zeichen lang sein',
+  max: '${path} darf höchstens ${max} Zeichen lang sein',
+  matches: '${path} muss wie folgt aussehen: "${regex}"',
+  email: '${path} muss eine gültige E-Mail-Adresse enthalten',
+  url: '${path} muss eine gültige URL sein',
+  trim: '${path} darf keine Leerzeichen am Anfang oder Ende enthalten',
+  lowercase: '${path} darf nur Kleinschreibung enthalten',
+  uppercase: '${path} darf nur Großschreibung enthalten',
+};
+
+export const number: LocaleObject['number'] = {
+  min: '${path} muss größer oder gleich ${min} sein',
+  max: '${path} muss kleiner oder gleich ${max} sein',
+  lessThan: '${path} muss kleiner sein als ${less}',
+  moreThan: '${path} muss größer sein als ${more}',
+  notEqual: '${path} darf nicht gleich sein mit "${notEqual}"',
+  positive: '${path} muss eine positive Zahl sein',
+  negative: '${path} muss eine negative Zahl sein',
+  integer: '${path} muss eine ganze Zahl sein',
+};
+
+export const date: LocaleObject['date'] = {
+  min: '${path} muss später sein als ${min}',
+  max: '${path} muss früher sein als ${max}',
+};
+
+export const boolean: LocaleObject['boolean'] = {};
+
+export const object: LocaleObject['object'] = {
+  noUnknown:
+    '${path}-Feld darf keine Schlüssel verwenden, die nicht im "Objekt-Shape" definiert wurden',
+};
+
+export const array: LocaleObject['array'] = {
+  min: '${path}-Feld muss mindesten ${min} Einträge haben',
+  max: '${path}-Feld darf höchstens ${max} Einträge haben',
+};
