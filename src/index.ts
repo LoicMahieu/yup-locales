8eafe26e3cfe0d59b22a07fd45ef27a3c65d3377
Gabriel Vaquer <brielov@icloud.com>
2020-08-13 18:10:42 -0300

    Add es to exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as nl from './locales/nl';
 import * as nb from './locales/nb';
 import * as de from './locales/de';
 import * as ar from './locales/ar';
+import * as es from './locales/es';
 
-export { fr, nl, nb, de, ar };
+export { fr, nl, nb, de, ar, es };
