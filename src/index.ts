746339db8f6c7e544620f1b6d03e43ad4e4d67ab
Loic Mahieu <mahieuloic@gmail.com>
2022-01-12 13:36:25 +0100

    index: export all locales

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import * as fr from './locales/fr';
-import * as nl from './locales/nl';
-import * as nb from './locales/nb';
-import * as de from './locales/de';
 import * as ar from './locales/ar';
-import * as it from './locales/it';
+import * as de from './locales/de';
 import * as es from './locales/es';
+import * as fr from './locales/fr';
+import * as he from './locales/he';
+import * as it from './locales/it';
+import * as nb from './locales/nb';
+import * as nl from './locales/nl';
 import * as pt from './locales/pt';
 
-export { fr, nl, nb, de, ar, es, it, pt };
+export { ar, de, es, fr, he, it, nb, nl, pt };
