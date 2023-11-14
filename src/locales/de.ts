6f3eb48623a1dab1c05209d35cd9fe9ddb0c1bee
Leonhard Melzer <leonhard.melzer@gmail.com>
2023-11-14 14:50:06 +0100

    Update de.ts
    Iterate de translations

--- src/locales/de.ts
+++ src/locales/de.ts
@@ -5,15 +5,15 @@ import { printValue, LocaleObject } from 'yup';
 // Based on https://github.com/jquense/yup/blob/b940eef48eb7456622ae384d0ffa7363d4fbad25/src/locale.ts
 export const mixed: LocaleObject['mixed'] = {
   default: '${path} ist ungültig.',
-  required: '${path} ist ein erforderliches Feld',
+  required: '${path} ist ein Pflichtfeld',
   defined: '${path} muss definiert werden',
-  notNull: '${path} kann nicht null sein',
+  notNull: '${path} darf nicht leer sein',
   oneOf: '${path} muss einer der folgenden Werte sein: ${values}',
   notOneOf: '${path} darf nicht einer der folgenden Werte sein: ${values}',
   notType: ({ path, type, value, originalValue }) => {
     const isCast = originalValue != null && originalValue !== value;
     let msg =
-      `${path} Muss ein \`${type}\` Typ sein, ` +
+      `${path} muss ein \`${type}\` Typ sein, ` +
       `Aber der Endwert war: \`${printValue(value, true)}\`` +
       (isCast
         ? ` (aus dem Wert \`${printValue(originalValue, true)}\` \`).`
@@ -30,44 +30,44 @@ export const mixed: LocaleObject['mixed'] = {
 };
 
 export const string: LocaleObject['string'] = {
-  length: '${path} muss genau ${length} Zeichen sein',
-  min: '${path} muss mindestens ${min} Zeichen sein',
-  max: '${path} muss höchstens ${max} Zeichen sein',
-  matches: '${path} muss Folgendes übereinstimmen: "${regex}"',
-  email: '${path} Muss eine gültige E -Mail sein',
-  url: '${path} Muss eine gültige URL sein',
-  uuid: '${path} muss ein gültiges UUID sein',
-  trim: '${path} Muss eine abgeschnittene Zeichenfolge sein',
-  lowercase: '${path} Muss eine Kleinbuchstabe sein',
-  uppercase: '${path} Muss eine obere Fallstring sein',
+  length: '${path} muss genau ${length} Zeichen lang sein',
+  min: '${path} muss mindestens ${min} Zeichen lang sein',
+  max: '${path} darf höchstens ${max} Zeichen lang sein',
+  matches: '${path} muss folgendes Muster haben: "${regex}"',
+  email: '${path} muss eine gültige E -Mail sein',
+  url: '${path} muss eine gültige URL sein',
+  uuid: '${path} muss eine gültige UUID sein',
+  trim: '${path} muss eine Zeichenfolge ohne Leerzeichen sein',
+  lowercase: '${path} muss in Kleinbuchstaben geschrieben sein',
+  uppercase: '${path} muss in Großbuchstaben geschrieben sein',
 };
 
 export const number: LocaleObject['number'] = {
-  min: '${path} muss größer sein als oder gleich ${min}',
-  max: '${path} muss geringer sein als oder gleich ${max}',
-  lessThan: '${path} muss geringer sein als ${less}',
-  moreThan: '${path} muss größer sein als ${more}',
+  min: '${path} muss größer oder gleich ${min} sein',
+  max: '${path} muss geringer oder gleich ${max} sein',
+  lessThan: '${path} muss kleiner als ${less} sein',
+  moreThan: '${path} muss größer als ${more} sein',
   positive: '${path} muss eine positive Zahl sein',
   negative: '${path} muss eine negative Zahl sein',
-  integer: '${path} muss eine Ganzzahl sein',
+  integer: '${path} muss eine ganze Zahl sein',
 };
 
 export const date: LocaleObject['date'] = {
-  min: '${path} Das Feld muss später als ${min} sein',
-  max: '${path} Das Feld muss früher als ${max} sein',
+  min: '${path} muss nach ${min} sein',
+  max: '${path} muss vor ${max} sein',
 };
 
 export const boolean: LocaleObject['boolean'] = {
-  isValue: '${path} Feld muss sein ${value}',
+  isValue: '${path} muss ${value} sein',
 };
 
 export const object: LocaleObject['object'] = {
   noUnknown:
-    '${path} Das Feld kann keine Schlüssel nicht in der Objektform angegeben haben',
+    '${path} kann keine unbekannten Werte enthalten',
 };
 
 export const array: LocaleObject['array'] = {
-  min: '${path} Feld muss mindestens ${min} Artikel haben',
-  max: '${path} Feld muss weniger als oder gleich ${max} Artikel haben',
-  length: '${path} muss ${length} Artikel haben',
+  min: '${path} muss mindestens ${min} Einträge haben',
+  max: '${path} darf höchstens ${max} Einträge haben',
+  length: '${path} muss ${length} Einträge haben',
 };
