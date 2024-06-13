3393edc0a6aae885b88cebc29f3db522fb7d759a
jacobmolby <jacob.moelby@gmail.com>
2024-06-13 16:26:17 +0200

    Update sv.ts

--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -4,17 +4,17 @@ import { printValue, LocaleObject } from 'yup';
 
 // Based on https://github.com/jquense/yup/blob/b940eef48eb7456622ae384d0ffa7363d4fbad25/src/locale.ts
 export const mixed: LocaleObject['mixed'] = {
-  default: '${path} är ogiltig.',
+  default: '${path} är ogiltig',
   required: '${path} är ett obligatoriskt fält',
   defined: '${path} måste definieras',
-  notNull: '${path} kan inte vara noll',
+  notNull: '${path} kan inte vara null',
   oneOf: '${path} måste vara ett av följande värden: ${values}',
   notOneOf: '${path} får inte vara ett av följande värden: ${values}',
   notType: ({ path, type, value, originalValue }) => {
     const isCast = originalValue != null && originalValue !== value;
     let msg =
-      `${path} måste vara en typ "${type}\`, ` +
-      `Men det slutliga värdet var: \`${printValue(value, true)}\`` +
+      `${path} måste vara en \`${type}\` typ, ` +
+      `men det slutliga värdet var: \`${printValue(value, true)}\`` +
       (isCast
         ? ` (gjutet från värdet \`${printValue(originalValue, true)}\`).`
         : '.');
@@ -34,12 +34,12 @@ export const string: LocaleObject['string'] = {
   min: '${path} måste vara minst ${min} tecken',
   max: '${path} måste vara högst ${max} tecken',
   matches: '${path} måste matcha följande: "${regex}"',
-  email: '${path} måste vara ett giltigt e -postmeddelande',
+  email: '${path} måste vara en giltig e-postadress',
   url: '${path} måste vara en giltig URL',
   uuid: '${path} måste vara en giltig UUID',
   trim: '${path} måste vara en trimmad sträng',
-  lowercase: '${path} måste vara en små bokstäver',
-  uppercase: '${path} Måste vara en stora sträng',
+  lowercase: '${path} måste vara små bokstäver',
+  uppercase: '${path} måste vara stora bokstäver',
 };
 
 export const number: LocaleObject['number'] = {
@@ -53,16 +53,16 @@ export const number: LocaleObject['number'] = {
 };
 
 export const date: LocaleObject['date'] = {
-  min: '${path} Fältet måste vara senare än ${min}',
-  max: '${path} Fältet måste vara tidigare än ${max}',
+  min: '${path} fältet måste vara senare än ${min}',
+  max: '${path} fältet måste vara tidigare än ${max}',
 };
 
 export const boolean: LocaleObject['boolean'] = {
-  isValue: '${path} Fältet måste vara ${value}',
+  isValue: '${path} fältet måste vara ${value}',
 };
 
 export const object: LocaleObject['object'] = {
-  noUnknown: '${path} Fältet kan inte ha nycklar som inte anges i objektformen',
+  noUnknown: '${path} fältet kan inte ha nycklar som inte anges i objektformen',
 };
 
 export const array: LocaleObject['array'] = {
